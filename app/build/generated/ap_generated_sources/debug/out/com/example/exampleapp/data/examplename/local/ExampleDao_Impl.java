2412e5d6a350c9c4d027a6ca90d5dca614bf1554
Hessu <hessu@kiho.fi>
2021-05-28 16:51:23 +0300

    Initial commit

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/exampleapp/data/examplename/local/ExampleDao_Impl.java
@@ -0,0 +1,60 @@
+package com.example.exampleapp.data.examplename.local;
+
+import android.database.Cursor;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import com.example.exampleapp.data.examplename.model.ExampleModel;
+import java.lang.Class;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class ExampleDao_Impl extends ExampleDao {
+  private final RoomDatabase __db;
+
+  public ExampleDao_Impl(RoomDatabase __db) {
+    this.__db = __db;
+  }
+
+  @Override
+  public List<ExampleModel> get() {
+    final String _sql = "SELECT * FROM ExampleModel";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfName = CursorUtil.getColumnIndexOrThrow(_cursor, "name");
+      final List<ExampleModel> _result = new ArrayList<ExampleModel>(_cursor.getCount());
+      while(_cursor.moveToNext()) {
+        final ExampleModel _item;
+        _item = new ExampleModel();
+        final int _tmpId;
+        _tmpId = _cursor.getInt(_cursorIndexOfId);
+        _item.setId(_tmpId);
+        final String _tmpName;
+        if (_cursor.isNull(_cursorIndexOfName)) {
+          _tmpName = null;
+        } else {
+          _tmpName = _cursor.getString(_cursorIndexOfName);
+        }
+        _item.setName(_tmpName);
+        _result.add(_item);
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
